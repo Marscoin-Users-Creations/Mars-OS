e4114e4e12c3be6917c2f0696a4e0607d50b3269
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2021-11-02 12:10:55 +0100

    Created gui.cpp

---
+++ C++/gui/gui.cpp
@@ -0,0 +1,66 @@
+namespace _GUI_ {
+    
+    int start() {
+        
+        create_rectangle();
+        
+    };
+    
+    int create_square(int pielxs_size, int x_position, int y_position, int borders_size, int multiple_intern_colors, int borders_color, int internet_colors) {
+        
+        int change_intern_color_after_x_px = 0;
+        if (multiple_intern_colors == 0) {
+            
+            
+            
+        } else {
+            
+            
+            
+        }
+        
+    };
+    int create_rectangle(int x_size, int y_size, int x_position, int y_position, int borders_size, int multiple_intern_colors, int borders_color, int intern_colors) {
+        
+        int change_intern_color_after_x_px = 0;
+        if (multiple_intern_colors == 0) {
+            
+            
+            
+        } else {
+            
+            
+            
+        }
+        
+    };
+    int create_triangle(int x_size, int x_position, int y_position, int borders_size, int multiple_inter_colors, int borders_color, int intern_colors) {
+        
+        int change_intern_color_after_x_px = 0;
+        if (multiple_intern_colors == 0) {
+            
+            
+            
+        } else {
+            
+            
+            
+        }
+        
+    };
+    int create_circle(int radius, int x_position, int y_position, int borders_size, int multiple_intern_colors, int borders_color, int intern_colors) {
+        
+        int change_intern_color_after_x_px = 0;
+        if (multiple_intern_colors == 0) {
+            
+            
+            
+        } else {
+            
+            
+            
+        }
+        
+    };
+    
+}
