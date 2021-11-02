df9418d3ea294eae6d2c72d26622f2b056decbae
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2021-11-02 12:33:09 +0100

    Created security.cpp

---
+++ C++/security/security.cpp
@@ -0,0 +1,79 @@
+#include "security.hpp"
+
+namespace _SECURITY_ {
+    
+    int leya() {
+        
+        static const consts = [];
+        int hash(int datas) {
+            
+            
+            
+        };
+        int brute_force(int hash) {
+            
+            int hashed_datas = ""
+            for (hashed_datas != hash) {
+                
+                
+                
+            };
+            
+        };
+        
+    };
+    int moss() {
+        
+        int decrypt() {
+            
+            
+            
+        };
+        int encrypt() {
+            
+            
+            
+        };
+        
+    };
+    int sha256() {
+        
+        int hash(int datas) {
+            
+            
+            
+        };
+        int brute_force(int hash) {
+            
+            int datas_hashed = ""
+            for (datas_hashed != hash) {
+                
+                
+                
+            };
+            
+        }
+        
+    };
+    int sha512() {
+        
+        int hash(int datas) {
+            
+            
+            
+        };
+        int brute_force(int hash) {
+            
+            int datas_hashed = ""
+            for (datas_hashed != hash) {
+                
+                
+                
+            };
+            return (datas)
+            
+        };
+        
+    };
+    
+}
