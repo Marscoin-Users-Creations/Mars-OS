f08daba3e883ff9ddc04aa7f83188c73d568c67b
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2021-11-02 12:16:55 +0100

    Created core.cpp

---
+++ C++/core.cpp
@@ -0,0 +1,29 @@
+namespace _CORE_ {
+    
+    int start() {
+        
+        
+        
+    };
+    
+    int date() {
+        
+        long int year = 0;
+        int month = 0;
+        int day = 0;
+        int hours = 0;
+        int minutes = 0;
+        int seconds = 0;
+        int deciseconds = 0;
+        long int centiseconds = 0;
+        long int milliseconds = 0;
+        
+        int now() {
+            
+            return (year +" " +month +" " +day +" " +hours +" " +minutes +" " +seconds +" " +milliseconds)
+            
+        };
+        
+    };
+    
+}
