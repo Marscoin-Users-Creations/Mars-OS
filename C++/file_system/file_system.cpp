c7878556ac3860c83a6a2d2adeb32e318a56be11
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2021-11-02 12:22:48 +0100

    Created file_system.cpp

---
+++ C++/file_system/file_system.cpp
@@ -0,0 +1,18 @@
+#include "file_system.hpp"
+
+namespace _FILE_SYSTEM_ {
+    
+    int user_file = read_file("A:\Users\ADMINISTRATOR\user.mosd");
+    
+    int create_file(int place, int type, int content) {
+        
+        
+        
+    };
+    int read_file(int place, int name) {
+        
+        
+        
+    };
+    
+}
