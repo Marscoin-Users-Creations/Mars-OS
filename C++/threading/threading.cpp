aed56138d8ac110d2d9c2320e6f411563b24b96a
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2021-11-02 15:42:21 +0100

    Updated threading.cpp

--- C++/threading/threading.cpp
+++ C++/threading/threading.cpp
@@ -2,24 +2,34 @@
 
 namespace _THREADING_ {
     
-    int next_thread_to_work = 0;
-    int thread = [{}, {}]
+    unsigned int next_thread_to_work = 0;
+    unsigned int threads = [{}, {}]
+    unsigned int threads_work_loads = [];
     
     int start() {
         
-        create_thread();
-        create_thread();
+        create_thread("nothing");
+        create_thread("nothing");
         
     };
     
     int create_thread(int work_to_do) {
         
-        thread.append("['works': '" +work_to_do +"']")
+        if (work_to_do == "nothing") {
+            
+            threads.append("['works': '" +repeat(1+1) +"']");
+            threads_work_loads.append(1);
+            
+        } else {
+            
+            threads.append("['works': '" +work_to_do +"']");
+            
+        };
         
     };
     int start_work(int work_to_do, int priority) {
         
-        thread[next_thread_to_work["works"]].append(work_to_do);
+        threads[(next_thread_to_work)["works"]].append(work_to_do);
         
     };
     
