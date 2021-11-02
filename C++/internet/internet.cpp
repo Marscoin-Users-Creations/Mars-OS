2135e373377e61e54083fb92f3e3a0a9e7fa1b51
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2021-11-02 12:26:25 +0100

    Created internet.cpp

---
+++ C++/internet/internet.cpp
@@ -0,0 +1,21 @@
+#include "internet.hpp"
+
+namespace _INTERNET_ {
+    
+    int send(int socket_address, int datas) {
+        
+        
+        
+    };
+    int recv(int server_address, int datas) {
+        
+        
+        
+    };
+    int close(int socket_address) {
+        
+        
+        
+    };
+    
+}
